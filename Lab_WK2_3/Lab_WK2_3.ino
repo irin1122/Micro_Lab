140cbcbb45eb692b94c07cd7762c8c3453c99061
irin1122 <62011122@kmitl.ac.th>
2020-02-02 01:35:02 +0700

    Create Lab_WK2_3.ino

---
+++ Lab_WK2_3/Lab_WK2_3.ino
@@ -0,0 +1,50 @@
+#define DATA_PIN 5
+#define CLOCK_PIN 6
+#define LATCH_PIN 4
+bool toggle;
+int c,i,val;
+
+void LED(uint16_t state)
+{
+  digitalWrite(LATCH_PIN,LOW);
+  for(i=0;i<16;i++)
+  {
+    digitalWrite(DATA_PIN,(state>>(15-i))&0x01);
+    digitalWrite(CLOCK_PIN,HIGH);
+    digitalWrite(CLOCK_PIN,LOW);
+  }
+  digitalWrite(LATCH_PIN,HIGH);
+}
+void setup() 
+{
+  pinMode(CLOCK_PIN,OUTPUT); 
+  pinMode(LATCH_PIN,OUTPUT); 
+  pinMode(DATA_PIN,OUTPUT);
+  pinMode(A0,INPUT_PULLUP);
+  pinMode(A1,INPUT_PULLUP);
+  pinMode(A2,INPUT);
+}
+
+void loop() 
+{
+    val = analogRead(A2);
+    LED(~(0x0001<<c));
+    if(c>15)
+    {
+      c=0;
+    }
+    if(c<0)
+    {
+      c=16;
+    }
+    if(((PINC>>0)&0x01)==0)
+    {
+      c++;
+    }
+    else if(((PINC>>1)&0x01)==0)
+    {
+      c--;
+    }
+    delay(val);
+
+}
